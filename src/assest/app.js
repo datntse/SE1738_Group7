200005e24e897ea5f393e20e860d78b61aa32324
datntse <dat.nt271102@gmail.com>
2023-06-01 14:53:12 +0700

    Upload [src] folder homepage.html

--- src/assest/app.js
+++ src/assest/app.js
@@ -24,10 +24,11 @@ $(document).ready(function(){
         slidesToScroll: 1,
         infinite: false,
         arrows: false,
-        // autoplay: true,
-        // autoplaySpeed: 1000,
+        autoplay: true,
+        autoplaySpeed: 1000,
         infinite: true,
-        arrows: true,
+        arrows: false,
+        dots: true,
         prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
         nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
       });
