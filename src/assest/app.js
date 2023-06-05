58b9f2532bc4d7b910e68e4a0b46e87fd57bbd29
datntse <dat.nt271102@gmail.com>
2023-06-05 10:25:38 +0700

    upload [src folder] with routing.html and update index.html

--- src/assest/app.js
+++ src/assest/app.js
@@ -32,4 +32,17 @@ $(document).ready(function(){
         prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
         nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
       });
+
+      $('.meal-wrapper').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: false,
+        arrows: true,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+      });
+
   });
