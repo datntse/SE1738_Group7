47bb978bcaa3bcee0c549da563b2deafc636166e
datntse <dat.nt271102@gmail.com>
2023-06-11 17:02:08 +0700

    upload [page.html] & [DB Diagram]

--- src/assest/app.js
+++ src/assest/app.js
@@ -46,3 +46,18 @@ $(document).ready(function(){
       });
 
   });
+
+  function increaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+  }
+  
+  function decreaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
+  }
