368469336b18c1c3c27fcbc30f332c3ce28bb8ca
Smilee <duybtvdu12354@gmail.com>
2023-06-12 22:44:24 +0700

    push html

--- src/assest/app.js
+++ src/assest/app.js
@@ -45,4 +45,16 @@ $(document).ready(function(){
         nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
       });
 
+      $('.bloglist-wrapper').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        infinite: false,
+        arrows: false,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+      });
+
   });
