d3458163c2f2f49931a98fb6392daeffc140fd77
V1c3z0 <lechiphat1090@gmail.com>
2023-06-17 23:09:34 +0700

    push homepage

--- src/back-end/BMOS/Controllers/HomeController.cs
+++ src/back-end/BMOS/Controllers/HomeController.cs
@@ -1,32 +1,50 @@
-﻿using BMOS.Models;
+﻿using BMOS.Models.Entities;
+using BMOS.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
-namespace BMOS.Controllers
+namespace Bmostest.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
+    public class HomeController : Controller
+    {
+        private readonly BmosContext _context;
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+        public HomeController(BmosContext context)
+        {
+            _context = context;
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        // GET: TblProducts
+        public async Task<IActionResult> Index()
+        {
+            return _context.TblProducts != null ?
+                        View(await _context.TblProducts.ToListAsync()) :
+                        Problem("Entity set 'BmosContext.TblProducts'  is null.");
+        }
+        public async Task<IActionResult> ChiTiet(string id)
+        {
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+            if (id == null || _context.TblProducts == null)
+            {
+                return NotFound();
+            }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+            var tblProduct = await _context.TblProducts
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (tblProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(tblProduct);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
